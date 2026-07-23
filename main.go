08f2b68645d82bc5cf1c14052c06331f0ff96ea9
Craig Stuckey <craigastuckey@gmail.com>
2026-07-23 10:00:07 -0500

    created handler for login command

--- main.go
+++ main.go
@@ -6,6 +6,31 @@ import (
 	"github.com/craigastuckey/gator/internal/config"
 )
 
+type state struct {
+	conf *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		err := fmt.Errorf("login requires a single argument: login <usersname>")
+		return err
+	}
+
+	err := s.conf.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("User has been set")
+
+	return nil
+}
+
 func main() {
 	cfg := config.Read()
 
